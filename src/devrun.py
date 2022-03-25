5d4bd4d14befb68b6864ba1f18b3e7797e96a9b4
Mickaël Buchet <mickael.buchet@m4x.org>
2022-03-25 11:01:21 +0100

    Fixed typos.

--- src/devrun.py
+++ src/devrun.py
@@ -1,6 +1,7 @@
 from lapdata import irLapData
 from dataclient import irDataClient
 import credentials
+import json
 
 # This file is for testing purposes. Comment the section that are not needed for your current testing.
 
@@ -12,17 +13,17 @@ results = client.get_result(45043469)
 laps = lpd.get_laps(45043469)
 
 # Dumping the results locally
-fr.open("test_results.json","w")
+fr = open("test_results.json","w")
 fr.write(json.dumps(results))
 fr.close()
-fd.open("test_laps.json","w")
+fd = open("test_laps.json","w")
 fd.write(json.dumps(laps))
 fd.close()
 
 # Reading the results from the local testing dump
-fr.open("test_results.json","r")
-fr.write(json.dumps(results))
+fr = open("test_results.json","r")
+res = json.loads(fr.read())
 fr.close()
-fd.open("test_laps.json","r")
-fd.write(json.dumps(laps))
+fd = open("test_laps.json","r")
+lap = json.loads(fd.read())
 fd.close()
