74818e7e402f3f1dd954bd56ec8da4bf486cccf1
Mickaël Buchet <mickael.buchet@m4x.org>
2022-04-01 00:10:43 +0200

    Typographic improvements.

--- src/handle_results.py
+++ src/handle_results.py
@@ -14,8 +14,8 @@ import jsonexport
 import argparse
 
 parser = argparse.ArgumentParser(description='Computes statistics for a race.')
-parser.add_argument('subsession_id', type=int, help="Session ID of the race")
-parser.add_argument('output_file', help="Name of the file to be written")
+parser.add_argument('subsession_id', type=int, help="Session ID of the race.")
+parser.add_argument('output_file', help="Name of the file to be written.")
 args = parser.parse_args()
 
 cred = credentials.get_credentials()
