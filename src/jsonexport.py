abb0e489946aba69ed06d4375431c168b131e1fd
Mickaël Buchet <mickael.buchet@m4x.org>
2022-04-03 00:26:12 +0200

    Added grades to the exported json file.

--- src/jsonexport.py
+++ src/jsonexport.py
@@ -16,6 +16,8 @@ def export_to_json(output_file, drivers_db, results, array_laps):
     drivers_statistics = merge_tables(race_table, qualifying_table)
     laps_statistics = laptools.get_lap_statistics(array_laps)
     drivers_statistics = merge_tables(drivers_statistics, laps_statistics)
+    grades = laptools.evaluate_drivers(array_laps)
+    drivers_statistics = merge_tables(drivers_statistics, grades)
     drivers_statistics = introduce_drivers_info(drivers_db, drivers_statistics)
     export = resulttools.get_race_infos(results)
     export["drivers_statistics"] = drivers_statistics
