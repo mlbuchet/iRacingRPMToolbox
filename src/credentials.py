69f160a62dfe9abfe51e0c340d7d89910b2251e2
Mickaël Buchet <mickael.buchet@m4x.org>
2022-04-02 11:14:56 +0200

    Added comments to credentias.py.

--- src/credentials.py
+++ src/credentials.py
@@ -1,4 +1,11 @@
+"""
+File encapsulating the management of credentials.
+"""
+
 import os
 
 def get_credentials():
+    '''
+    Obtains the credentials from environment variables.
+    '''
     return {"user":os.getenv('IRAPI_USR'), "pwd":os.getenv('IRAPI_PWD')}
