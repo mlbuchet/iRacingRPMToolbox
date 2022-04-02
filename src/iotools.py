e4aed437c49c326f21526d04da56d7be92f5e042
Mickaël Buchet <mickael.buchet@m4x.org>
2022-04-02 15:18:54 +0200

    Added function to export a list of selected laps in very short form.

--- src/iotools.py
+++ src/iotools.py
@@ -1,3 +1,23 @@
-import pandas
+"""
+File containing function to export data in formats other than json.
+"""
 
-#Import/Export CSV to be implemented.
+import pandas
+import leaguetools
+
+"""
+Export to CSV
+"""
+
+"""
+Export to text files
+"""
+def export_noted_laps(file, noted_laps, drivers_db):
+    '''
+    Export a list of laps numbers associated with the driver's name.
+    '''
+    fw = open(file, "w")
+    noted_laps.sort(key=lambda x: int(x["lap_number"]))
+    for lap in noted_laps:
+        fw.write(str(lap["lap_number"])+'\t'+leaguetools.find_driver(drivers_db, lap["cust_id"])["name"]+"\n")
+    fw.close()
