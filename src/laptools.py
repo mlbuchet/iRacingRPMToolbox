ea128c7df955f62a04de5567db8bdbca0b626ce2
Mickaël Buchet <mickael.buchet@m4x.org>
2022-04-02 14:04:17 +0200

    Removed outdated functions from laptools.

--- src/laptools.py
+++ src/laptools.py
@@ -2,49 +2,6 @@
 File containing the functions working on laps and array of laps.
 """
 
-def get_best_lap(array_laps, cust_id = None):
-    '''
-    Returns the fastest lap of a driver from an array of laps.
-    Requires an array of laps without untimed laps.
-    If no id is provided for the driver, it returns the fastest lap of the race augmented with the id of the driver having done it.
-    Only clean laps are counted.
-
-    WARNING: Calling this function without customer id will modifiy the returned lap with an extra field of data.
-    '''
-    # Looking for the fastest overall lap
-    if cust_id == None:
-        minlap = array_laps[0]["laps"][0]
-        fastest_id = array_laps[0]["cust_id"]
-        for laps in array_laps:
-            for lap in laps["laps"]:
-                if lap["lap_time"] <= minlap["lap_time"] and lap["lap_events"] == []:
-                    minlap = lap
-                    fastest_id = laps["cust_id"]
-        minlap["cust_id"] = fastest_id
-    # Looking for the fastest lap from a given driver
-    else:
-        for laps in array_laps:
-            if laps["cust_id"] == cust_id and len(laps["laps"]) > 0:
-                minlap = laps["laps"][0]
-                for lap in laps["laps"]:
-                    if lap["lap_time"] <= minlap["lap_time"] and lap["lap_events"] == []:
-                        minlap = lap
-    return minlap
-
-def sort_laps(laps):
-    '''
-    Sorts an array of laps and return the sorted array.
-    '''
-    aux = []
-    for lap in laps:
-        aux.append((lap["lap_time"], lap))
-    aux.sort(reverse = True)
-    sorted = []
-    while aux:
-        sorted.append(aux.pop()[1])
-    return sorted
-
-
 def compute_average(laps, top = None):
     '''
     Computes the average time of the best laps of an array.
@@ -54,7 +11,7 @@ def compute_average(laps, top = None):
     '''
     if top == None:
         top = len(laps)
-    sorted = sort_laps(laps)
+    laps.sort(key=lambda x: int(x["lap_time"]))
     index = 0
     sum = 0
     while index < top:
