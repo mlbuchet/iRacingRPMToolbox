d6ac2ce16b4e1801500535e4d8267447aa283e10
Mickaël Buchet <mickael.buchet@m4x.org>
2022-04-02 11:26:32 +0200

    Improved comments in leaguetools.py.

--- src/leaguetools.py
+++ src/leaguetools.py
@@ -3,6 +3,14 @@ File containing the function working on the league table.
 """
 
 def build_drivers_database(league):
+    '''
+    Creates a drivers database from the league information.
+    The database contains the following fields:
+    cust_id: id of the driver.
+    car_nuber: number of the driver.
+    name: name of the driver without extra numbers or other artifact.
+    class: class in which the driver competes.
+    '''
     db = []
     for driver in league["roster"]:
         entry = {"cust_id":driver["cust_id"], "car_number":driver["car_number"]}
