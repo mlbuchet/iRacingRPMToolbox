7ba3c36754af18a19d9761911ae5268053f6d94e
Mickaël Buchet <mickael.buchet@m4x.org>
2023-05-22 21:36:04 +0200

    chore: set up to do list for the main command wrapper program

--- src/iRacingToolbox.py
+++ src/iRacingToolbox.py
@@ -1,4 +1,17 @@
 import click
 
+
+@click.command()
+@click.option('--session-id', help='Session number of the race', required=True)
+def compute_results(session_id):
+    click.secho(f"Yep it's empty but your id is {session_id}")
+
+
 if __name__ == '__main__':
-    pass
+    compute_results()
+
+
+# TODO Rewrite the client to use httpx instead of requests for the api calls
+# TODO Make an inventory of all use cases
+# TODO Reimplement use cases using pandas
+# TODO Move to poetry for the management of dependencies
